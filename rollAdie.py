f433e15ea936fcdd6695c8e8675b20cd93ede9f8
tkdtanishq <tanishq12122007@gmail.com>
2026-01-05 22:51:30 +0530

    Game(Non-Graphic):A Die

---
+++ rollAdie.py
@@ -0,0 +1,13 @@
+# WAP to stimulate the rolling of a die,
+# also ask the user if they want to continue or not.
+import random
+print("Welcome to the Rolling A die, make your choice :D\n")
+while True:
+    choice=input("Yes/Enter: if you want to continue\nNo: if you want to exit\nYour choice:")
+    if choice=="Yes" or choice=='yes' or choice=='':
+        print(f"Your number is:{random.randint(1,6)}\n")
+    elif choice=="No" or choice=="no":
+        print("Exiting...")
+        break
+    else:
+        print("Please enter Yes or No")
